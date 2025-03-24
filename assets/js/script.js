8deef0ec0ff7e1696bd2b0efe007c69824f0c903
tapiagr7502 <tapiagr7502@uwec.edu>
2025-03-24 11:56:19 -0500

    Added NavBar, Footer and product page

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+// assets/js/script.js
+window.addEventListener("DOMContentLoaded", () => {
+    fetch("../components/navbar.html")
+      .then(response => response.text())
+      .then(data => {
+        document.getElementById("navbar").innerHTML = data;
+      });
+  
+    fetch("../components/footer.html")
+      .then(response => response.text())
+      .then(data => {
+        document.getElementById("footer").innerHTML = data;
+      });
+});
